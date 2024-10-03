ea725a38f136d2f50d30fe92ae30693ad0ca0f34
sharabh mishra <sharabhmishra15@gmail.com>
2024-10-03 17:00:08 +0530

    generating a random math problem with solution

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import http from "http";
 import path from "path";
 import cors from "cors";
 import { Server } from "socket.io";
+import randomMathQuestion from "random-math-question";
 
 const app = express();
 const server = http.createServer(app);
@@ -15,6 +16,27 @@ const io = new Server(server, {
 
 app.use(cors());
 
+app.get("/", async (req, res) => {
+  var mathQuestion = randomMathQuestion.get({
+    numberRange: "1-10",
+    amountOfNumber: "2-2",
+    operations: [ "*", "+", "-"],
+    nagative: {
+      containsNagatives: false,
+      negativeChance: "0%",
+    },
+    exponent: {
+      containsExponent: false,
+      exponentChance: "0%",
+      // exponentRange: "1-10",
+    },
+  });
+  res
+    .status(200)
+    .json({ question: mathQuestion.question, answer: mathQuestion.answer });
+});
+
+// socket.io stuff
 io.on("connection", (socket) => {
   console.log("A new user has connected", socket.id);
 
