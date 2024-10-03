fe0bd4837ca0d38687134c65fa12f95822c1dc02
sharabh mishra <sharabhmishra15@gmail.com>
2024-10-04 00:39:44 +0530

    frontend setup to work with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,67 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { io } from "socket.io-client";
+import axios from "axios";
 
 function App() {
   const [socket, setSocket] = useState("");
+  const [newQuestion, setNewQuestion] = useState("");
   const [answer, setAnswer] = useState("");
+  const [winner, setWinner] = useState(null);
+  const [highScore, setHighScore] = useState(0);
+  const [countdown, setCountdown] = useState(0);
+  const [activeUsers, setActiveUsers] = useState(0);
+  // const [newQuestionCheck, setNewQuestionCheck] = useState(false); //boolean for whether the question is new or old
+
+  const baseUrl = import.meta.env.VITE_BACKEND_URL;
+
+  // async function fetchQuestion() {
+  //   const response = await axios.get(baseUrl); //remove later*****************************************************
+  //   setNewQuestion(response?.data?.question);
+  // }
 
   useEffect(() => {
+    // fetchQuestion();
+    // establish a dual modality connection with backend
     const newSocket = io.connect("http://localhost:9000");
     setSocket(newSocket);
 
+    newSocket.emit("request-question");
+
+    newSocket.on("new-question", (data) => {
+      setNewQuestion(data?.question);
+      setWinner(null); //winners will be reset
+      setAnswer("");
+      setCountdown(0);
+    });
+
+    newSocket.on("winner-announcement", (data) => {
+      setWinner(data?.winner);
+      setCountdown(data?.countdown);
+      if (data?.winner === newSocket.id) {
+        setHighScore((prev) => prev + 1);
+      }
+    });
+
+    newSocket.on("active-users", (data) => {
+      setActiveUsers(data.count);
+    });
+
     return () => newSocket.disconnect();
   }, []);
+
+  useEffect(() => {
+    let timer;
+    if (countdown > 0) {
+      timer = setInterval(() => {
+        setCountdown((prev) => prev - 1);
+      }, 1000);
+    }
+
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [countdown]);
 
   function handleChange(e) {
     setAnswer(e.target.value);
@@ -25,11 +75,15 @@ function App() {
       console.log("answer wasn't submitted");
     }
   }
+
+  // console.log(winner, socket.id);
+
   return (
     <>
       <div className="container">
+        <h1>active users: {activeUsers}</h1>
         <section className="dialogue-box">
-          <h1>what is 1 + 1?</h1>
+          <h1>what is {newQuestion}?</h1>
           <input
             className="input-box"
             type="text"
@@ -37,7 +91,15 @@ function App() {
             value={answer}
             placeholder="your answer"
           />
-          <button onClick={handleClick}>submit</button>
+          <button onClick={handleClick} disabled={countdown > 0}>
+            submit
+          </button>
+          {winner && (
+            <p>{winner === socket?.id ? "You won!" : `${winner} won!`}</p>
+          )}
+
+          {countdown > 0 && <p>Next question in {countdown} seconds</p>}
+          <p>Your high score: {highScore}</p>
         </section>
       </div>
     </>
